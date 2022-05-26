7481a4b4dbca50cce28b4c4becae7f64e6927f1c
VankaVenkatesh <vvenkatesh257@gmail.com>
2022-05-26 07:48:37 +0530

    JS values and variables assignment is complted

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -6,13 +6,38 @@
  * [ ] Finally, check the amount against your bank account balance to see if you can afford it or not.
 */
 
-const SPENDING_THRESHOLD = 200;
+const SPENDING_THRESHOLD = 300;
 const TAX_RATE = 0.08;
 const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
 var amount = 0;
+var purchase_amount = 0;
 // your code goes here
 
 // ⛑ Answer of the above will `$334.76`.
+
+
+for (let i=0;amount < bank_balance;i++) {
+    amount += PHONE_PRICE;
+    if (amount > bank_balance) {
+        purchase_amount = amount-PHONE_PRICE;
+    }
+    if (purchase_amount < SPENDING_THRESHOLD){
+        purchase_amount = purchase_amount + ACCESSORY_PRICE;
+    }
+}
+purchase_amount = purchase_amount + purchase_amount*TAX_RATE
+alert(purchase_amount);
+
+purchase_amount<bank_balance ? alert("You can afford") : alert("You cannot afford")
+
+// while (amount<bank_balance) {
+//     amount += PHONE_PRICE;
+//     PHONE_PRICE++ ;
+    
+    
+// }
+
+// alert(amount);
